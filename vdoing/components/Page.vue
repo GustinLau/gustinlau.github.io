a9f27b1c5845c8bc935ffa4294759b02dc4f5df4
GustinLau <gustinlau@gmail.com>
2022-04-27 18:02:52 +0800

    2022/04/27

--- vdoing/components/Page.vue
+++ vdoing/components/Page.vue
@@ -19,16 +19,10 @@
             />
             <span>
               {{ $page.title }}
-              <span v-if="linkedTitle" class="link" @click="openExternalLink"><svg xmlns="http://www.w3.org/2000/svg"
-                                                                                   aria-hidden="true" focusable="false"
-                                                                                   x="0px" y="0px" viewBox="0 0 100 100"
-                                                                                   width="16" height="16"
-                                                                                   class="icon outbound"><path
-                fill="currentColor"
-                d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon
-                fill="currentColor"
-                points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg> <span
-                class="sr-only">(opens new window)</span></span>
+              <span v-if="linkedTitle" class="link" @click="openExternalLink">
+                 <OutboundLink/>
+                <span class="sr-only">(opens new window)</span>
+              </span>
             </span>
             <span class="title-tag" v-if="$frontmatter.titleTag">
               {{ $frontmatter.titleTag }}
@@ -131,7 +125,14 @@ export default {
 .theme-vdoing-wrapper
   .content-wrapper
     position relative
-
+  .digest
+    display none
+  .footnotes
+    .footnote-item
+      margin-top 0
+      padding-top 0
+      p
+        margin-top .2rem
   h1
     .link
       cursor pointer
