29d4ba029d7600a74dfa3d997342e54d02b8a5d3
GustinLau <gustinlau@gmail.com>
2022-06-02 21:01:42 +0800

    删除无用依赖

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -161,25 +161,25 @@ const config = {
       }
     ],
     // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-    [
-      'demo-block',
-      {
-        settings: {
-          // 在线示例(jsfiddle, codepen)中的js依赖
-          // jsLib: ['http://xxx'],
-          // 在线示例中的css依赖
-          // cssLib: ['http://xxx'],
-          // 在线示例中的vue依赖
-          // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js',
-          // 是否显示 jsfiddle 链接
-          jsfiddle: false,
-          // 是否显示 codepen 链接
-          codepen: true,
-          // 是否展示为横向样式
-          horizontal: false
-        }
-      }
-    ],
+    // [
+    //   'demo-block',
+    //   {
+    //     settings: {
+    //       // 在线示例(jsfiddle, codepen)中的js依赖
+    //       // jsLib: ['http://xxx'],
+    //       // 在线示例中的css依赖
+    //       // cssLib: ['http://xxx'],
+    //       // 在线示例中的vue依赖
+    //       // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js',
+    //       // 是否显示 jsfiddle 链接
+    //       jsfiddle: false,
+    //       // 是否显示 codepen 链接
+    //       codepen: true,
+    //       // 是否展示为横向样式
+    //       horizontal: false
+    //     }
+    //   }
+    // ],
     // 放大图片
     [
       'vuepress-plugin-zooming',
@@ -248,13 +248,6 @@ const config = {
     //     }
     //   }
     // ],
-    // 百度统计 （你可以去掉）
-    // [
-    //   'vuepress-plugin-baidu-tongji',
-    //   {
-    //     hm: baiduCode || '503f098e7e5b3a5b5d8c5fc2938af002',
-    //   },
-    // ],
     // 本地插件（供学习）
     // 鼠标点击爱心特效
     // [
@@ -265,38 +258,6 @@ const config = {
     //     // 要排除元素的class, 默认空''
     //     excludeClassName: 'theme-vdoing-content'
     //   }
-    // ],
-    // 百度自动推送
-    // 'vuepress-plugin-baidu-autopush',
-    // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-    // [
-    //   'thirdparty-search',
-    //   {
-    //     thirdparty: [
-    //       // 可选，默认 []
-    //       {
-    //         title: '在MDN中搜索',
-    //         frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-    //         behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-    //       },
-    //       {
-    //         title: '在Runoob中搜索',
-    //         frontUrl: 'https://www.runoob.com/?s=',
-    //       },
-    //       {
-    //         title: '在Vue API中搜索',
-    //         frontUrl: 'https://cn.vuejs.org/v2/api/#',
-    //       },
-    //       {
-    //         title: '在Bing中搜索',
-    //         frontUrl: 'https://cn.bing.com/search?q=',
-    //       },
-    //       {
-    //         title: '通过百度搜索本站的',
-    //         frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
-    //       },
-    //     ],
-    //   },
     // ],
   ],
   // markdown配置
